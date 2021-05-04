317db63f568f12c460d3bd70776112c2fb9b5656
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2021-05-04 09:13:54 -0300

    Day-8

--- day-6/function-while-loop/main.py
+++ day-6/function-while-loop/main.py
@@ -1,17 +1,19 @@
-def print_name(name):
-    print(f"Hi {name}")
-
-
-name = input("What is your name?\n")
-
-print_name(name)
-
-###
-
-
 def press_y_to_exit():
     key = "n"
     while key != "y":
+        def print_name(param_name, param_surname):
+            print(f"Hi {param_surname}, {param_name} ")
+
+        name = input("What is your name?\n")
+        surname = input("What is your surname?\n")
+
+        print("Passando os argumentos respeitando o POSITIONAL")
+        print_name(name, surname)
+
+        print("Passando os argumentos respeitando o KEYWORD ARGUMENT")
+        print_name(param_surname=surname, param_name=name)
+
         key = input("press y to exit\n")
+
 
 press_y_to_exit()
