d0c368b0ece94047059a8ffae8911527b0af3711
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2021-05-13 07:23:16 -0300

    Day-20 & Day-21

--- day-20/snake-game/main.py
+++ day-20/snake-game/main.py
@@ -38,10 +38,8 @@ while game_is_on:
         scoreboard.game_over()
 
     # Detect collision with tail.
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:  # usando slice, tratará tudo na lista que não for o primeiro item
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 
