ebda01e015ea8b92624005704077b7a640316bc4
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2021-05-13 07:08:23 -0300

    Day-20 & Day-21

--- day-20/snake-game/food.py
+++ day-20/snake-game/food.py
@@ -2,10 +2,11 @@ from turtle import Turtle
 import random
 
 
+# Turtle é a superclasse. A maneira que fazemos herança em Python é colocando a superclasse dentro de ()
 class Food(Turtle):
 
     def __init__(self):
-        super().__init__()
+        super().__init__()  # É recomendado utilizar o super(), quando fazemos herança mas não é obrigatório
         self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
